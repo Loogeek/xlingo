54197b2a93860a34adf33c6cedb586f5f9af9c19
Loogeek <xiaolong3956@gmail.com>
2024-09-02 23:22:24 +0800

    feat: add learn page

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,5 +1,21 @@
+import { FeedWrapper } from "@/components/FeedWrapper";
+import { StickyWrapper } from "@/components/StickyWrapper";
+import { UserProgress } from "@/components/UserProgress";
+
 const LearnPage = () => {
-  return <div>learn</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        {/* <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={isPro}
+        /> */}
+      </StickyWrapper>
+      <FeedWrapper>feed wrapper</FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
