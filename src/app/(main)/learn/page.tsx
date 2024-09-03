641be4388c17b443d5b45d5a563610b4b3920090
Loogeek <xiaolong3956@gmail.com>
2024-09-03 18:15:45 +0800

    feat: add unit

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,16 +1,30 @@
 import { FeedWrapper } from "@/components/FeedWrapper";
 import { StickyWrapper } from "@/components/StickyWrapper";
 import { UserProgress } from "@/components/UserProgress";
-import { getUnits, getUserProgress } from "@/../db/queries";
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from "@/../db/queries";
 import { redirect } from "next/navigation";
 import { Header } from "./header";
+import { lessons, units as unitsSchema } from "@/../db/schema";
+import { Unit } from "./unit";
 
 const LearnPage = async () => {
-  const [userProgress, units] = await Promise.all([
-    getUserProgress(),
-    getUnits(),
-  ]);
+  const [userProgress, units, courseProgress, lessonPercentage] =
+    await Promise.all([
+      getUserProgress(),
+      getUnits(),
+      getCourseProgress(),
+      getLessonPercentage(),
+    ]);
   if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  if (!courseProgress) {
     redirect("/courses");
   }
 
@@ -18,7 +32,25 @@ const LearnPage = async () => {
     <div className="flex  gap-[48px] px-6">
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
-        {JSON.stringify(units)}
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={
+                courseProgress.activeLesson as
+                  | (typeof lessons.$inferSelect & {
+                      unit: typeof unitsSchema.$inferSelect;
+                    })
+                  | undefined
+              }
+              activeLessonPercentage={lessonPercentage}
+            />
+          </div>
+        ))}
       </FeedWrapper>
       <StickyWrapper>
         <UserProgress
